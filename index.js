3bfede34e3a668b95bde00f5e8d6a216b7a57213
JonHimself <jcbrstudio@gmail.com>
2021-08-19 14:15:44 -0500

    Search Function Prototype Implmented

--- index.js
+++ index.js
@@ -2,50 +2,22 @@ let tickerValue = document.querySelector('.tickerValue');
 let priceAnima = document.querySelector('.priceAni');
 
 
-// const searchTicker = () => {
-//     let name = document.querySelector('#name')
-//     let price = document.querySelector('#price');
-//     let ticker = document.querySelector('.search').value;
-    
-//     axios.get(`https://api.nomics.com/v1/currencies/ticker?key=9ae301e9467e3eef04cb5868e1c8ed92299dc5d3&ids=${ticker}`)
-//     .then(res => { 
-//         while(name.firstChild) {
-//             name.removeChild(name.firstChild)
-//         };
-//         while(price.firstChild) {
-//             price.removeChild(price.firstChild)
-//         }
-//         axios.get(`https://api.nomics.com/v1/currencies/ticker?key=9ae301e9467e3eef04cb5868e1c8ed92299dc5d3&ids=${ticker}`)
-//         .then(res => {
-//             for(let i = 0; i <= res.data.length; i++) {
-//                 let currentID = res.data[i].id;
-//                 let idHolder = document.createElement('p');
-//                 idHolder.classList.add('id');
-//                 idHolder.textContent = currentID;
-//                 name.appendChild(idHolder);
-//             }
-//         })
-//         axios.get(`https://api.nomics.com/v1/currencies/ticker?key=9ae301e9467e3eef04cb5868e1c8ed92299dc5d3&ids=${ticker}`)
-//         .then(res => {
-//             for(let i = 0; i <= res.data.length; i++) {
-//                 let currentPrice = res.data[i].price;
-//                 let priceHolder = document.createElement('p');
-//                 priceHolder.classList.add('price');
-//                 priceHolder.textContent = currentPrice;
-//                 price.appendChild(priceHolder);
-//             }
-//         })
-//     })
-// }
+
+const searchTicker = () => {
+let runId = setTimeout(updateId, 3500)
+let runAni = setInterval(priceAnimation, 3500);
+let runPrice = setInterval(updatePrice, 3500);
+}
 
 
 
 const updateId = () => {
+    let ticker = document.querySelector('.search').value;
     let name = document.querySelector('#name')
     while(name.firstChild) {
         name.removeChild(name.firstChild)
     }
-    axios.get('https://api.nomics.com/v1/currencies/ticker?key=9ae301e9467e3eef04cb5868e1c8ed92299dc5d3&ids=XMR')
+    axios.get(`https://api.nomics.com/v1/currencies/ticker?key=9ae301e9467e3eef04cb5868e1c8ed92299dc5d3&ids=${ticker}`)
     .then(res => {
         for(let i = 0; i <= res.data.length; i++) {
             let currentID = res.data[i].id;
@@ -58,8 +30,9 @@ const updateId = () => {
 }
 
 const updatePrice = () => {
+    let ticker = document.querySelector('.search').value;
     let price = document.querySelector('#price');
-    axios.get(`https://api.nomics.com/v1/currencies/ticker?key=9ae301e9467e3eef04cb5868e1c8ed92299dc5d3&ids=XMR`)
+    axios.get(`https://api.nomics.com/v1/currencies/ticker?key=9ae301e9467e3eef04cb5868e1c8ed92299dc5d3&ids=${ticker}`)
     .then(res => {
         for(let i = 0; i <= res.data.length; i++) {
             let currentPrice = res.data[i].price;
@@ -68,31 +41,10 @@ const updatePrice = () => {
     })
 }
 
-//First Price Function but created flickering problem
-
-// const updatePrice = () => {
-//     let price = document.querySelector('#price');
-//     while(price.firstChild) {
-//         price.removeChild(price.firstChild);
-//     }
-//     axios.get(`https://api.nomics.com/v1/currencies/ticker?key=9ae301e9467e3eef04cb5868e1c8ed92299dc5d3&ids=XMR`)
-//     .then(res => {
-//         for(let i = 0; i <= res.data.length; i++) {
-//             let currentPrice = res.data[i].price;
-//             let priceHolder = document.createElement('p');
-//             priceHolder.classList.add('price');
-//             priceHolder.textContent = currentPrice;
-//             price.appendChild(priceHolder);
-//         }
-//     })
-// }
 
 const priceAnimation = () => {
     priceAnima.classList.toggle('priceAni')
 }
 
 
-// tickerValue.addEventListener('click', searchTicker);
-let runId = setTimeout(updateId, 3000)
-let runAni = setInterval(priceAnimation, 3000);
-let runPrice = setInterval(updatePrice, 3000);
+tickerValue.addEventListener('click', searchTicker);
