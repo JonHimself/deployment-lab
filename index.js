f7ff62430d499ba86da62601ab7ec58380ca96c0
JonHimself <jcbrstudio@gmail.com>
2021-08-19 21:49:42 -0500

    Styled a bit before implementing ticker list. Added image removal in this commit

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 let tickerValue = document.querySelector('.tickerValue');
 let priceAnima = document.querySelector('.priceAni');
-let body = document.querySelector('body')
-
-
+let imgContainer = document.querySelector('#img-container')
+// let cryptoNameContainer = document.querySelector('#crypto-name-container');
 
 let runAni;
 let runPrice;
@@ -10,13 +9,36 @@ let runPrice;
 const searchTicker = () => {
 clearInterval(runAni);
 clearInterval(runPrice);
-let runId = setTimeout(updateId, 3000);
-runAni = setInterval(priceAnimation, 4000); 
-runPrice = setInterval(updatePrice, 4000);
-// bodyImg();
+let runId = setTimeout(updateId, 2000);
+let runImg = setTimeout(bodyImg, 2000)
+runAni = setInterval(priceAnimation, 3000); 
+runPrice = setInterval(updatePrice, 3000);
 }
+// const tickerList = () => {
+//     axios.get(`https://api.nomics.com/v1/currencies/ticker?key=9ae301e9467e3eef04cb5868e1c8ed92299dc5d3`)
+//     .then(res => {
+//         for(let i = 0; i < res.data.length; i++) {
+//             let tickerids = res.data[i].id;
+//             imgContainer.appendChild(imgHolder);
+//         }
+//     })
+// }
 
-
+const bodyImg = () => {
+    let ticker = document.querySelector('.search').value;
+    while(imgContainer.firstChild){
+        imgContainer.removeChild(imgContainer.firstChild)
+    }
+    axios.get(`https://api.nomics.com/v1/currencies/ticker?key=9ae301e9467e3eef04cb5868e1c8ed92299dc5d3&ids=${ticker}`)
+    .then(res => { 
+        for(let i = 0; i < res.data.length; i++) {
+            let img = res.data[i].logo_url;
+            let imgHolder = document.createElement('img');
+            imgHolder.setAttribute('src', `${img}`);
+            imgContainer.appendChild(imgHolder);
+        }
+    })
+}
 
 const updateId = () => {
     let ticker = document.querySelector('.search').value;
@@ -25,7 +47,7 @@ const updateId = () => {
         name.removeChild(name.firstChild)
     }
     axios.get(`https://api.nomics.com/v1/currencies/ticker?key=9ae301e9467e3eef04cb5868e1c8ed92299dc5d3&ids=${ticker}`)
-    .then(res => { console.log(res.data)
+    .then(res => {
         for(let i = 0; i < res.data.length; i++) {
             let currentID = res.data[i].id;
             let idHolder = document.createElement('p');
@@ -36,18 +58,6 @@ const updateId = () => {
     })
 }
 
-// const bodyImg = () => {
-//     axios.get(`https://api.nomics.com/v1/currencies/ticker?key=9ae301e9467e3eef04cb5868e1c8ed92299dc5d3&ids=${ticker}`)
-//     .then(res => { console.log(res.data)
-//         for(let i = 0; i < res.data.length; i++) {
-//             let img = res.data[i].logo_url;
-//             let imgHolder = document.createElement('p');
-//             imgHolder.setAttribute('href', img)
-//             body.appendChild(imgHolder);
-//         }
-//     })
-// }
-
 const updatePrice = () => {
     let tickerPrice = document.querySelector('#name').textContent;
     let price = document.querySelector('#price');
@@ -56,11 +66,10 @@ const updatePrice = () => {
     .then(res => {
         for(let i = 0; i < res.data.length; i++) {
             let currentPrice = res.data[i].price;
-            price.textContent = currentPrice;
+            price.textContent = `$ ${currentPrice}`;
         }
     })
     }
-
 
 const priceAnimation = () => {
     priceAnima.classList.toggle('priceAni')
